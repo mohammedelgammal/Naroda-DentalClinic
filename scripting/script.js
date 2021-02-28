eb1b891688792a4e0fd67b41279f50c8921e3133
someone <someone@someemail.com>
2021-02-28 23:07:20 +0200

    fully responsive

--- scripting/script.js
+++ scripting/script.js
@@ -9,10 +9,41 @@ window.addEventListener('scroll', () => {
     }
 });
 
-const swiperSlider = new Swiper('.swiper-container', {
+const closer = document.querySelector('.closer');
+const menu = document.querySelector('.floating-menu');
+let hamburger = document.querySelector('.left-hand-content svg');
+
+closer.addEventListener('click', () => {
+    closer.style.animation = 'fadeout 0.5s ease';
+    closer.style.opacity = 0;
+    closer.style.zIndex = -1;
+    menu.style.transform = 'translateX(-400px)';
+    hamburger.style.transform = 'rotateZ(0deg)';
+});
+
+hamburger.addEventListener('click', () => {
+    if(closer.style.animation === 'fadein 0.5s ease'||
+    closer.style.opacity === 0.3||
+    closer.style.zIndex === 9||
+    menu.style.transform === 'translateX(0px)'||hamburger.style.transform === 'rotateZ(90deg)'){
+        closer.style.animation = 'fadeout 0.5s ease';
+        closer.style.opacity = 0;
+        closer.style.zIndex = -1;
+        menu.style.transform = 'translateX(-400px)';
+        hamburger.style.transform = 'rotateZ(0deg)';
+    } else {
+        closer.style.animation = 'fadein 0.5s ease';
+        closer.style.opacity = 0.3;
+        closer.style.zIndex = 9;
+        menu.style.transform = 'translateX(0px)';
+        hamburger.style.transform = 'rotateZ(90deg)';
+    }
+});
+
+let swiperSlider = new Swiper('.swiper-container', {
     loop: true,
     slidesPerView: 3,
-    spaceBetween: 60,
+    spaceBetween: 50,
     pagination: {
         el: '.swiper-pagination',
         clickable: true
@@ -23,5 +54,27 @@ const swiperSlider = new Swiper('.swiper-container', {
         delay: 2500,
         disableOnInteraction: false,
     },
-    freeMode: false
+    freeMode: false,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.05,
+            spaceBetween: 20
+        },
+        688: {
+            slidesPerView: 2,
+            spaceBetween: 50
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 50
+        },
+        1312: {
+            slidesPerView: 3.5,
+            spaceBetween: 50
+        },
+        1500: {
+            slidesPerView: 5.5,
+            spaceBetween: 50
+        }
+    }    
 });
